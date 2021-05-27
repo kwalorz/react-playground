5b0b47bd57f350714e55f933dbecf74f73abe7ef
kwalorz <kmwalorz@gmail.com>
2021-05-27 15:07:26 -0400

    created reusable cards

--- my-app/src/components/App.js
+++ my-app/src/components/App.js
@@ -3,6 +3,16 @@ import Header from "./Header";
 import Logo from "./Logo";
 import Card from "./Card";
 
+let data = {
+  fullName: "Kevin Walorz",
+  status: "friend",
+  description: "World Class react Developer",
+  totalFriends: "100",
+  joinedYear: "2021",
+  imageUrl:
+    "https://images.unsplash.com/photo-1621897100070-055b183ead92?ixid=MnwxMjA3fDB8MHx0b3BpYy1mZWVkfDE4fDZzTVZqVExTa2VRfHxlbnwwfHx8fA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60",
+};
+
 export default function () {
   return (
     <>
@@ -18,13 +28,7 @@ export default function () {
         style={{ margin: "20px auto", width: "100%", maxWidth: "960px" }}
       ></main>
       <div className="ui link cards">
-        <Card />
-        <Card />
-        <Card />
-        <Card />
-        <Card />
-        <Card />
-        <Card />
+        <Card userData={data} />
       </div>
     </>
   );
--- my-app/src/components/Card.js
+++ my-app/src/components/Card.js
@@ -4,22 +4,22 @@ export default function (props) {
   return (
     <div className="card">
       <div className="image">
-        <img src="https://images.unsplash.com/photo-1518242340236-fd1dd715ba89?ixid=MnwxMjA3fDB8MHx0b3BpYy1mZWVkfDl8NnNNVmpUTFNrZVF8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60" />
+        <img src={props.userData.imageUrl} />
       </div>
       <div className="content">
-        <div className="header">Matt Giampietro</div>
+        <div className="header">{props.userData.fullName}</div>
         <div className="meta">
-          <a>Friends</a>
+          <a>{props.userData.status}</a>
         </div>
-        <div className="description">
-          Matthew is an interior designer living in New York.
-        </div>
+        <div className="description">{props.userData.description}</div>
       </div>
       <div className="extra content">
-        <span className="right floated">Joined in 2013</span>
+        <span className="right floated">
+          Joined in {props.userData.joinedYear}
+        </span>
         <span>
           <i className="user icon" />
-          75 Friends
+          {props.userData.totalFriends}
         </span>
       </div>
     </div>
